bdc711feecf5b1f0c007180de033061f61d2ca48
Joseph Grounds <jgrounds@Josephs-MacBook-Pro-2.local>
2018-07-23 07:34:15 -0600

    commit

--- src/container/Recommend/RecommendLayout.js
+++ src/container/Recommend/RecommendLayout.js
@@ -6,36 +6,35 @@ import IndustryForm from './IndustryForm'
 import PriceForm from './PriceForm'
 
 class RecommendLayout extends Component {
-    constructor(props) {
-      super(props);
-      this.getState = this.getState.bind(this);
-      this.state = {
-        answer:[]
-      }
-    }
-      
-    componentWillMount(){
-       QuestionsStore.on("post_answer", this.getState);
-    }
 
-    getState(){
-      this.setState({answer: QuestionsStore.getState()});
+  constructor(props) {
+    super(props);
+    this.getState = this.getState.bind(this);
+    this.state = {
+      answer:[]
     }
+  }
+    
+  componentWillMount(){
+      QuestionsStore.on("post_answer", this.getState);
+  }
 
-    renderQuestions(){
-      console.log('renderQuestion fn = ',!this.state.answer.size)
-      if(!this.state.answer.size){
-        return <IndustryForm/>
-      }
-      else{
-        return <PriceForm/>
-      }
+  getState(){
+    this.setState({answer: QuestionsStore.getState()});
+  }
+
+  renderQuestions(){
+    console.log('renderQuestion fn = ',!this.state.answer.size)
+    if(!this.state.answer.size){
+      return <IndustryForm/>
     }
+    else{
+      return <PriceForm/>
+    }
+  }
 
-  
   render() {
     const { answer } = this.state
-    // console.log('render', answer)
       return (
         <div className="recommend-bg">
           {this.renderQuestions()}
